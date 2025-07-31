bac19a6df85a50bf4a3fe722b153ebe9bb05f3b4
JamesBirchall-dev <jamesbdorel@gmail.com>
2025-07-31 19:48:56 +0100

    change: quiz.js & ei.html to bootstrap validation and new questions

--- assets/script/quiz.js
+++ assets/script/quiz.js
@@ -1,31 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => { 
     const form = document.querySelector("form");
-    const nextBtn = document.querySelector("#next"); 
     const sectionKey = document.body.dataset.section; 
     const feedbackImage = document.getElementById("section-result");
 
-nextBtn.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     
-    //set for questions, the answers and to target radio elements
-    const questions = new Set();
-    const answered = new Set();
-    const inputs = form.querySelectorAll("input[type=radio]");
+    form.classList.add("was-validated");
 
-    //loop for each input element and add names to questions
-    inputs.forEach(input => {
-        questions.add(input.name);
-    // adding question name to the set of answers
-        if (input.checked) {
-            answered.add(input.name);
-        }
-    })
-
-// validate calculation
- if (questions.size !== answered.size) {
-    alert("Please answer all questions before continuing");
-    return;
- }
+    if (!form.checkValidity()) {
+        form.classList.add("was-validated");
+        return;
+    }
+ 
  alert("Form complete ready to store")//testing form validation and store functionality. 
 });
-});
+})
