d51ff9482dcd8b16ab8ad9aadda2422d2b848134
JamesBirchall-dev <jamesbdorel@gmail.com>
2025-07-31 20:12:32 +0100

    update: ei.html & quiz.js - bottom of form -validation message

--- assets/script/quiz.js
+++ assets/script/quiz.js
@@ -2,17 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("form");
     const sectionKey = document.body.dataset.section; 
     const feedbackImage = document.getElementById("section-result");
+    const validationMsg = document.getElementById("validation-message"); 
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    form.classList.add("was-validated");
-
-    if (!form.checkValidity()) {
+    // Listen for form submission and prevent reload of page
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); // 
+        //bootstrap class
         form.classList.add("was-validated");
-        return;
-    }
- 
- alert("Form complete ready to store")//testing form validation and store functionality. 
+
+        // if form is invalid, displays message as block
+        if (!form.checkValidity()) {
+            if (validationMsg) {
+                validationMsg.style.display = "block"; 
+            }
+            return; 
+        }
+
+        // If the form is valid displays no message
+        if (validationMsg) {
+            validationMsg.style.display = "none"; 
+        }
+
+        alert("Form complete — ready to store");
+    });
 });
-})
